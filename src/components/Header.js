3a353b033c88c5c85ea37f33cdf6ff4844faccf6
anant_dixit_13 <anantdixit100@gmail.com>
2025-05-13 13:04:05 +0530

    pakka

---
+++ src/components/Header.js
@@ -0,0 +1,75 @@
+import {
+  AppBar,
+  Container,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import {
+  createTheme,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import { useHistory } from "react-router-dom";
+import { CryptoState } from "../CryptoContext";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    flex: 1,
+    color: "white",
+    fontFamily: "'Orbitron', sans-serif",
+    fontWeight: "bold",
+    cursor: "pointer",
+    textShadow: "0 0 10px rgba(110, 21, 21, 0.5)",
+    letterSpacing: "2px",
+  },
+}));
+
+const darkTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#fff",
+    },
+    type: "dark",
+  },
+});
+
+function Header() {
+  const classes = useStyles();
+  const { currency, setCurrency } = CryptoState();
+
+  const history = useHistory();
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => history.push(`/`)}
+              variant="h6"
+              className={classes.title}
+            >
+              Crypto Tracker
+            </Typography>
+
+            <Select
+              variant="outlined"
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={currency}
+              style={{ width: 100, height: 40, marginLeft: 15 }}
+              onChange={(e) => setCurrency(e.target.value)}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"INR"}>INR</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+}
+
+export default Header;
